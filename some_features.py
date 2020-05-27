35725e70e1e17198bd585a3a673278341fe1f5c6
Mostafa <s.mostafa.a96@gmail.com>
2020-05-28 01:52:22 +0430

    elementwise multiplication

--- some_features.py
+++ some_features.py
@@ -5,3 +5,11 @@ a1 = torch.ones([4, 5])
 v1 = torch.randn([1, 5])
 b1 = torch.tensor(10)
 print(a1 @ v1.t() + b1)
+
+################## elementwise multiplication
+
+a2 = torch.ones([4, 5])
+a3 = torch.randn([4, 5])
+v2 = torch.randn([1, 5])
+print(a2 * v2)
+print(a2 * a3)
