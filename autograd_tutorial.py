94820aa321c8bdff4ec228c8a1d3ae225774e53f
Mostafa <s.mostafa.a96@gmail.com>
2020-05-27 22:29:40 +0430

    more details with grad_fn

--- autograd_tutorial.py
+++ autograd_tutorial.py
@@ -3,12 +3,13 @@ from torch_test import MyTorchTest
 
 m = MyTorchTest()
 x = torch.ones(2, 2, requires_grad=True)
+xd = x.detach()
+m.assertEqual(xd.grad_fn, None)
 y = x * x * 9
-y.backward(x)
-z = x * 18
-
 with torch.no_grad():
-    y = y ** 2
-
-# d(9 * x^2)/dx = 18 * x
+    yd = y ** 2
+m.assertEqual(yd.grad_fn, None)
+y.backward(x)
+# d(9 * x^2 * 10)/dx = 180 * x
+z = x * 18
 m.tensorAssertEqual(z, x.grad)
