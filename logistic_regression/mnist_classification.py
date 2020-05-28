aadf2e30739883c21e4828239e923718e8eb6c76
Mostafa <s.mostafa.a96@gmail.com>
2020-05-28 16:51:48 +0430

    just code, not tested

--- logistic_regression/mnist_classification.py
+++ logistic_regression/mnist_classification.py
@@ -1,9 +1,11 @@
+import torch
 from torch import nn
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data.dataloader import DataLoader
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 from small_unittest_for_torch import MyTorchTest
+import torch.nn.functional as F
 import numpy as np
 
 
@@ -18,22 +20,46 @@ test_dataset = MNIST(root='./data/', train=False)
 tensor_of_image, label = dataset[0]
 m = MyTorchTest()
 m.tensorAssertShape(tensor_of_image, (1, 28, 28))
-train_indices, test_indices = split_train_and_test_indices(len(dataset), test_percentage=0.2)
-batch_size = 100
-train_sampler = SubsetRandomSampler(train_indices)
-train_loader = DataLoader(dataset, batch_size, sampler=train_sampler)
-test_sampler = SubsetRandomSampler(test_indices)
-test_loader = DataLoader(dataset, batch_size, sampler=test_sampler)
 
 
 class LogisticRegressionModel(nn.Module):
-    def __init__(self, in_features, out_features):
+    def __init__(self, x: torch.Tensor, y: torch.Tensor, batch_size=100):
         super().__init__()
+        in_features = x.size()[1] * x.size()[2]
+        out_features = len(y.unique())
         self.linear = nn.Linear(in_features=in_features, out_features=out_features)
+        self.train_indices, self.test_indices = split_train_and_test_indices(len(dataset),
+                                                                             test_percentage=0.2)
+        self.batch_size = batch_size
 
-    def forward(self, xb):
-        xb = xb.reshape(-1, self.linear.in_features)
-        return self.linear(xb)
+    @staticmethod
+    def accuracy(t1: torch.Tensor, t2: torch.Tensor):
+        return torch.sum(t1 == t2).item() / len(t1)
+
+    def accuracy_on_train(self):
+        test_sampler = SubsetRandomSampler(self.test_indices)
+        test_loader = DataLoader(dataset, self.batch_size, sampler=test_sampler)
+        all_trues = 0
+        for xb, yb in test_loader:
+            outs = self.linear(xb)
+            probs = F.softmax(outs, dim=1)
+            max_probs, preds = torch.max(probs, dim=1)
+            all_trues += torch.sum(preds == yb)
+        return all_trues / len(self.test_indices)
+
+    def train(self, epochs=500, step=1e-5):
+        opt = torch.optim.SGD(self.parameters(), lr=step)
+        for i in range(epochs):
+            train_sampler = SubsetRandomSampler(self.train_indices)
+            train_loader = DataLoader(dataset, self.batch_size, sampler=train_sampler)
+            for xb, yb in train_loader:
+                xb = xb.reshape(-1, self.linear.in_features)
+                outs = self.linear(xb)
+                # bellow function named cross entropy has implemented softmax internally and maps labels to one-hot arrays(i.e 9:[0,0,0,0,0,0,0,0,0,1]).# noqa
+                loss = F.cross_entropy(outs, yb)
+                loss.backward()
+                opt.step()
+                opt.zero_grad()
 
 
-model = LogisticRegressionModel(tensor_of_image.size()[-1] ** 2, 10)
+# model = LogisticRegressionModel(tensor_of_image.size()[-1] ** 2, 10)
