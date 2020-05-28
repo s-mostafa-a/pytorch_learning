ee7b4b7213534a8f87ae0d6fcc7e98e1c3958548
Mostafa <s.mostafa.a96@gmail.com>
2020-05-28 19:00:39 +0430

    minor reshape

--- scattered_contents_from_all_tutorials/scattered_from_pytorch_tutorial.py
+++ scattered_contents_from_all_tutorials/scattered_from_pytorch_tutorial.py
@@ -1,17 +1,21 @@
 import torch
 from small_unittest_for_torch import MyTorchTest
 
-################## training how to initialize
+##################
+# how to initialize
 x = torch.tensor([5.5, 3])
-# print(x)
+print(x)
 y = x.new_ones((2, 2, 2, 2))
-# print(y)
-z = x.new_ones(5, 3, dtype=torch.int64)
-# print(z)
+print(y)
+z = x.new_ones(5, 3, dtype=torch.float32)
+print(z)
 a = torch.randn_like(z, dtype=torch.float)
-# print(a)
-################## training operators
-#### checking all shapes of add
+print(a)
+
+##################
+# operators
+####
+# checking all shapes of add
 
 res1 = a + z
 res2 = torch.add(a, z)
@@ -23,7 +27,8 @@ m.tensorAssertEqual(a, res1)
 m.tensorAssertEqual(a, res2)
 m.tensorAssertEqual(a, res3)
 
-#### checking shape and in-place operands
+####
+# checking shape and in-place operands
 old_shape = z.size()
 z_t = z.t()
 m.tensorAssertShape(z, old_shape)
@@ -31,7 +36,8 @@ m.tensorAssertShape(z, old_shape)
 z.t_()
 m.tensorAssertShape(z, z_t.shape)
 
-#### resize
+####
+# resize
 x = torch.randn(4, 4)
 y = x.view(16)
 z = x.view(-1, 8)
@@ -39,7 +45,8 @@ m.tensorAssertShape(x, (4, 4))
 m.tensorAssertShape(y, (16,))
 m.tensorAssertShape(z, (2, 8))
 
-################## numpy bridge
+##################
+# numpy bridge
 b = a.numpy()
 c = torch.from_numpy(b)
 m.tensorAssertEqual(a, c)
@@ -47,8 +54,15 @@ a.add_(1)
 m.tensorAssertEqual(a, c)
 # it shows that there is always a shallow copy on numpy bridges
 
-################## pytorch and matplot differences
-# pytorch expects color channels to be first dimension of tensor. but matplot expects it to be the last one (or not being displayed at all) # noqa
+##################
+# pytorch and matplot differences
+# pytorch expects color channels to be first dimension of tensor. but matplot expects it to
+#   be the last one (or not being displayed at all)
 
-################## parameters of model
-# assume a model which inherits nn.module class. model.parameter shows all parameters that model has got (weights, biases, etc)  # noqa
+##################
+# parameters of model
+# assume a model which inherits nn.module class. model.parameter shows all parameters that
+#   model has got (weights, biases, etc)  # noqa
+
+##################
+#
