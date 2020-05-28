f65b0e9e1d61a23c1d71acb7bc43713b75cc751d
Mostafa <s.mostafa.a96@gmail.com>
2020-05-28 14:21:12 +0430

    imshow crashes with commented codes :|

--- logistic_regression/mnist_classification.py
+++ logistic_regression/mnist_classification.py
@@ -1,5 +1,24 @@
 import torch
 import torchvision
 from torchvision.datasets import MNIST
-dataset = MNIST('./data/', download=True)
-len(dataset)
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from small_unittest_for_torch import MyTorchTest
+
+# dataset = MNIST(root='./data/', train=True, transform=transforms.ToTensor())
+# test_dataset = MNIST(root='./data/', train=False)
+# tensor_of_image, label = dataset[0]
+# m = MyTorchTest()
+# m.tensorAssertShape(tensor_of_image, (1, 28, 28))
+# nt = tensor_of_image.view(28 * 28)
+# nt[nt.argmax()] = 0.000
+# nt = nt.view(1, 28, 28)
+# plt.imshow(tensor_of_image[0, 10:15, 10:15], cmap='gray')
+# plt.show()
+# print(nt)
+
+dataset = MNIST(root='./data/', train=True)
+test_dataset = MNIST(root='./data/', train=False)
+tensor_of_image, label = dataset[0]
+plt.imshow(tensor_of_image, cmap='gray')
+plt.show()
