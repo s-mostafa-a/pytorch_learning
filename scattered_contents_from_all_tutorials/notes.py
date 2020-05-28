b2e2de9b4c45f062867a3a57c9533fb91a266b2e
Mostafa <s.mostafa.a96@gmail.com>
2020-05-29 01:10:02 +0430

    adds usage of data sampler and data loader to notes

--- scattered_contents_from_all_tutorials/notes.py
+++ scattered_contents_from_all_tutorials/notes.py
@@ -1,5 +1,22 @@
 import torch
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import Dataset
+
 from small_unittest_for_torch import MyTorchTest
+
+
+class MyCustomDataSet(Dataset):
+    def __init__(self, dataset, targets):
+        self.dataset = dataset
+        self.targets = targets
+
+    def __len__(self):
+        return len(self.targets)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx], self.targets[idx]
+
 
 ##################
 # how to initialize
@@ -65,4 +82,17 @@ m.tensorAssertEqual(a, c)
 #   model has got (weights, biases, etc)  # noqa
 
 ##################
-#
+# sampler and data loader
+data_to_be_sampled = torch.ones((20, 10))
+target_to_be_sampled = torch.ones((20, 1))
+whole_data = MyCustomDataSet(data_to_be_sampled, target_to_be_sampled)
+whole_data.targets = target_to_be_sampled
+whole_data.data = data_to_be_sampled
+
+train_sampler = SubsetRandomSampler([0, 2, 4, 6, 8, 10, 12, 14])
+train_loader = DataLoader(whole_data, batch_size=1, sampler=train_sampler)
+for xb, yb in train_loader:
+    print(xb)
+    print(len(xb))
+    print(yb)
+    print(len(yb))
