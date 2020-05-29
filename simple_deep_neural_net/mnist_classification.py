2f6945eb4ac34a96d554b07e2ad168d8d25f2672
Mostafa <s.mostafa.a96@gmail.com>
2020-05-29 10:23:55 +0430

    starting deep neural network stuffs

---
+++ simple_deep_neural_net/mnist_classification.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.dataloader import DataLoader
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+from small_unittest_for_torch import MyTorchTest
+import torch.nn.functional as F
+
+
+class DNNModel(nn.Module):
+    pass
+
+
+trains = MNIST(root='./data/', train=True, transform=transforms.ToTensor())
+tests = MNIST(root='./data/', train=False, transform=transforms.ToTensor())
